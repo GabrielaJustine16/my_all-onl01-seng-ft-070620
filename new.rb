6af50b0eab517be3dec5369af6131233485654ab
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-22 05:56:18 +0000

    Automatically backed up by Learn

--- new.rb
+++ new.rb
@@ -1,6 +1,9 @@
 def my_all?(collection)
   i = 0
   while i < collection.length
-    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+    
+    yield (collection[i])
+    
+    i+=1
   end
 end
